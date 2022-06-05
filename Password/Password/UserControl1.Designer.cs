66ae97a9571e8d8fcd12e9b90bb5baffba5befbf
Abdul Mateen <98536971+ligeetah@users.noreply.github.com>
2022-06-05 21:58:50 +0500

    Add files via upload

---
+++ Password/Password/UserControl1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Password
+{
+    partial class PassTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblchk = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblchk
+            // 
+            this.lblchk.AutoSize = true;
+            this.lblchk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblchk.Location = new System.Drawing.Point(205, 11);
+            this.lblchk.Name = "lblchk";
+            this.lblchk.Size = new System.Drawing.Size(48, 17);
+            this.lblchk.TabIndex = 1;
+            this.lblchk.Text = "Invalid";
+            // 
+            // PassTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblchk);
+            this.Controls.Add(this.textBox1);
+            this.Name = "PassTest";
+            this.Size = new System.Drawing.Size(262, 41);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblchk;
+    }
+}
---
+++ Password/Password/UserControl1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password
+{
+    public partial class PassTest: UserControl
+    {
+        public PassTest()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length<4)
+            {
+                lblchk.Text = "InValid";
+            }
+            else
+            {
+                lblchk.Text = "Valid";
+            }
+        }
+    }
+}
