53f1caab3cc35ea8fa82d9fc2e60d4bd89b6416e
Abdul Mateen <98536971+ligeetah@users.noreply.github.com>
2022-06-05 21:53:26 +0500

    Add files via upload

---
+++ Timer/Timer/UserControl1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class CustomTimer: UserControl
+    {
+        private int count=100;
+        public event EventHandler OnStop1;
+        public int Count { get { return count; } set { count = value; } }
+        public CustomTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count--;
+            if(count>0)
+            {
+                label1.Text = count.ToString();
+            }
+            else if(count==0)
+            {
+                MessageBox.Show("Time Over!");
+                OnStop1?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void CustomTimer_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled=true;
+        }
+        /*public void OnStop()
+        {
+            MessageBox.Show("END");
+        }*/
+    }
+}
---
+++ Timer/timertest/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace timertest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customTimer1 = new Timer.CustomTimer();
+            this.SuspendLayout();
+            // 
+            // customTimer1
+            // 
+            this.customTimer1.Count = 67;
+            this.customTimer1.Location = new System.Drawing.Point(161, 48);
+            this.customTimer1.Name = "customTimer1";
+            this.customTimer1.Size = new System.Drawing.Size(276, 262);
+            this.customTimer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customTimer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Timer.CustomTimer customTimer1;
+    }
+}
+
